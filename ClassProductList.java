9993ef6f8e89bce2c3d1e578c7817383a8e42f23
manjubhargavisudanagunta <112654700+manjubhargavisudanagunta@users.noreply.github.com>
2022-10-13 19:57:00 -0700

    Individual Assignment Design Pattern Skeleton commit

---
+++ ClassProductList.java
@@ -0,0 +1,13 @@
+public class ClassProductList {
+
+	private Reminder Visitor reminder Visitor;
+
+	private Product[] product;
+
+	private ProductIterator productIterator;
+
+	public void accept(NodeVisitor visitor) {
+
+	}
+
+}
---
+++ OfferingIterator.java
@@ -0,0 +1,21 @@
+public class OfferingIterator {
+
+	private OfferingList offeringList;
+
+	public boolean hasNext() {
+		return false;
+	}
+
+	public Product Next() {
+		return null;
+	}
+
+	public void MoveToHead() {
+
+	}
+
+	public void Remove() {
+
+	}
+
+}
