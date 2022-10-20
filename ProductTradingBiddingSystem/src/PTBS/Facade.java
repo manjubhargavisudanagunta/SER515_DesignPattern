f043f36b083e8155be8d2ce0679bf2af5f74a175
manjubhargavisudanagunta <112654700+manjubhargavisudanagunta@users.noreply.github.com>
2022-10-19 23:10:30 -0700

    Update Facade.java

--- ProductTradingBiddingSystem/src/PTBS/Facade.java
+++ ProductTradingBiddingSystem/src/PTBS/Facade.java
@@ -1,106 +1,157 @@
+package PTBS;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class ptbs {
+public class Facade implements Reminder{
 
-	private static Object ProductList;
+	private int UserType;
 
-	public static void main(String[] args) throws IOException {
-		
-		System.out.println("Facade ");
-		Facade facade = new Facade();
-		Map<String,Object> authenticationResponse = facade.login();
-		if((boolean) authenticationResponse.get("loginStatus")) {
-			System.out.println("Welcome "+authenticationResponse.get("thePerson"));
-			System.out.println(" 0  to continue");
-			System.out.println("1 to logout");
+	@SuppressWarnings("unused")
+	private Product theSelectedProduct;
+	
+	@SuppressWarnings("unused")
+	private int nProductCategory;
+
+	@SuppressWarnings("unused")
+	private ClassProductList theProductList;
+
+	private String thePerson;
+
+	public Map<String,Object> login() {
+		boolean login_Status = false;
+		Map<String, Object> authenticationMap = new HashMap<>();
+		try {
+			System.out.println("Enter Username:");
 			@SuppressWarnings("resource")
-			Scanner sc = new Scanner(System.in);
-			int consoleLog = sc.nextInt();
-			if(consoleLog == 1) {
-				System.out.println("Logged out Successfully");
-				facade.login();
-			}else if(consoleLog == 0){
-				System.out.println("Iterator ");
-				String inputProductFile = "./src/PTBS/ProductInfo.txt";
-				FileReader ProductInfoFile;
-				String[] productList = new String[15];
-				int i = 0;
-				try {
-					ProductInfoFile = new FileReader(inputProductFile);
-					@SuppressWarnings("resource")
-					BufferedReader ProdRead = new BufferedReader(ProductInfoFile);
-				    String product = ProdRead.readLine();
-				    while (product != null) {
-				    	productList[i] = product;
-				    	i++;
-				    	product = ProdRead.readLine();
-				    }
-				} catch (FileNotFoundException e) {
-					System.out.println("Error,try again!");
-				}
-				ProductIterator ci = new ProductIterator(ProductList);
-				System.out.println("Select the  Produc: ");
-				ci.createIterator();
-				while(ci.hasNext()) {
-					ci.next().forEach((key, value) -> System.out.println(" "+key + " : " + value));
-				}
-				int ProductChoice = sc.nextInt();
-				String selectedProduct = productList[ProductChoice-1];
-				System.out.println("select Product Level "+selectedProduct);
-				System.out.println(" 0 : ProduceProductMenu");
-				System.out.println(" 1 : MeatProductMenu");
-				int choice = sc.nextInt();
-				String ProductLevel = (choice == 1) ? "MeatProductMenu" : "ProduceProductMenu";
-				System.out.println("Choose one  ");
-				System.out.println(" 0 : Create Product Menu");
-				System.out.println("1 : Show Menu");
-				System.out.println("2 : Reminder");
-				System.out.println("3 : Logout");
-				int option = sc.nextInt();
-				if(option == 0) {
-					System.out.println(" Factory ");
-					
-					if(true) {
-						new Seller(null).CreateProductMenu(authenticationResponse.get("thePerson"),selectedProduct, ProductLevel);
-					}else {
-						new Buyer(null).CreateProductMenu(authenticationResponse.get("thePerson"),selectedProduct, ProductLevel);
-					}
-				}else if(option == 1) {
-				 System.out.println("Show Product Menu");
-				}else if(option == 2) {
-					System.out.println("Reminder");
-				}else if(option == 3) {
-					System.out.println("Logged out Successfully!");
-					facade.login();
-				}else {
-					System.out.println("Invalid option");
-				}
-				System.out.println("\nDo you want to proceed?");
-				System.out.println(" 0 : NO");
-				System.out.println("1 : YES");
-				int confirm = sc.nextInt();
-				if(confirm == 0) {
-					System.out.println("Cancelled.");
-				}else if(confirm == 1) {
-					System.out.println("Bridge");
-					System.out.println("Create Product menu ");
-					System.out.println("Send product level touser");
-					
-					 Person seller = new Seller(new MeatProductMenu());
-				     seller.ProductMenu();
-
-				     System.out.println("\nVisitor");
-				}else {
-					System.out.println("Invalid option, please try again!");
-				}
-			}else {
-				System.out.println("Invalid option, please try again!");
+			Scanner pm = new java.util.Scanner(System.in);
+			String user1 = pm.nextLine();
+			System.out.println("Enter Password:");
+			String password1 = pm.nextLine();
+		    boolean Login = loginVerification(".src/PTBS/SellerInfo.txt", user1, password1);
+		    if(Login) {
+		    	System.out.println("Logged in as a Seller");
+		    	login_Status = Login;
+		    	UserType = Login? 0:1;
+		    	thePerson = user1;
+		    }else{
+		    	boolean BuyerLoginStatus = loginVerification(".src/PTBS/BuyerInfo.txt", user1, password1);
+		    	if(BuyerLoginStatus) {
+		    		System.out.println("logged in as Buyer ");
+			    	login_Status = BuyerLoginStatus;
+			    	UserType = BuyerLoginStatus? 1:0;
+			    	thePerson = user1;
+		    	}else {
+					throw new Exception("Try again");
+			    }
 			}
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+			login();
 		}
+		authenticationMap.put("userType", UserType);
+		authenticationMap.put("theperson", thePerson);
+		authenticationMap.put("loginStatus", login_Status);
+		return authenticationMap;
+	}
+	
+	public boolean loginVerification(String inputFileType, String User1, String pw1) {
+		boolean login_Status = false;
+		try {
+	    	boolean userFound = false;
+	    	FileReader credentialFile = new FileReader(inputFileType);
+	    	@SuppressWarnings("resource")
+			BufferedReader readsellerInfo = new BufferedReader(credentialFile);
+		    String sell = readsellerInfo.readLine();
+		    while (sell != null) { 
+		    String user2 = sell.substring(0,sell.indexOf(":"));
+            if (User1.equalsIgnoreCase(user2))  {
+            	userFound = true;
+            	String password2 = sell.substring(sell.lastIndexOf(":") + 1);
+            	boolean passwordCheck = pw1.equals(password2);
+            	if(passwordCheck) {
+            		System.out.println("Login success");
+            		login_Status = true;
+            	}else {
+            		System.out.println("Invalid Username/Password");
+            		login();
+            	}
+             }
+             sell = readsellerInfo.readLine();
+            }
+		    if(!userFound) {
+		    	return userFound;
+		    }
+	    }catch (FileNotFoundException e) {
+			System.out.println("File Handling Exception Occured");
+			login();
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+			login();
+		}
+		return login_Status;
+	}
+	
+	public void selectProduct() {
+		System.out.println("Select the Product");
 		
 	}
+	public void addTrading() {
+		System.out.println("Add Trading");
+	}
+
+	public void viewTrading() {
+		System.out.println("View Trading");
+	}
+
+	public void decideBidding() {
+
+	}
+
+	public void discussBidding() {
+
+	}
+
+	public void submitBidding() {
+
+	}
+
+	public void remind() {
+		System.out.println("Reminder");
+	}
+
+	public void createUser(UserInfoItem userinfoitem) {
+
+	}
+
+	public void createProductLIst() {
+
+	}
+
+	public void AttachProductToUser() { 
+	}
+
+	public Product SelectProduct() {
+		return null;
+	}
+
+	public void productOperation() {
+
+	}
+
+	@Override
+	public int visit(Product product) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int visit(Trading trading) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
